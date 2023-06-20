62043c9356b332e02e702dfda2938d042d21a843
Sergio Olivo <sergio_oliver009@hotmail.com>
2023-06-20 08:31:52 -0500

    pokemon

--- src/Home/Chart.jsx
+++ src/Home/Chart.jsx
@@ -35,7 +35,7 @@ const Chart = ({ pokemon }) => {
     ];
 
     return (
-        <ResponsiveContainer width="50%" height="50%">
+        <ResponsiveContainer width="100%" height="100%">
             <RadarChart cx="50%" cy="50%" outerRadius="70%" data={data}>
                 <PolarGrid />
                 <PolarAngleAxis dataKey="subject" />
