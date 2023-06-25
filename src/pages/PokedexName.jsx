4e2ee331f4b32c5ad1c9f398ee26128b48a265c7
Sergio Olivo <sergio_oliver009@hotmail.com>
2023-06-25 13:57:25 -0500

    act

--- src/pages/PokedexName.jsx
+++ src/pages/PokedexName.jsx
@@ -27,64 +27,71 @@ const PokedexName = () => {
                             <h1 className="text_fail_pokemon">😢 the pokemon "<span className="searchNameFail">{name}</span>" doesn't exist'</h1>
                         </div>
                     : (
+                        <div className="cont">
                         <article className="selector">
-                            <div className="cardSearch">
-                                <div className="imgcontain">
-                                    <img className='img_pokemon_search' src={pokemon?.sprites.other['official-artwork'].front_default} alt="" />
-                                </div>
-                                <div className="boxNumber">
-                                    <p className="number_Pokemon">#{pokemon?.id}</p>
-                                </div>
-                                <h2 className='name_pokemon_selector'>{name}</h2>
-                                <div className="line"></div>
-                                <ul className="pokemon_weight_height">
-                                    <div className="div_1">
-                                        <p className="wh">WEIGHT</p>
-                                        <li className='weight'>{pokemon?.weight}</li>
+                            <section className='section_pokemon_search'>
+                                <div className="cardSearch">
+                                    <div className="imgcontain">
+                                        <img className='img_pokemon_search' src={pokemon?.sprites.other['official-artwork'].front_default} alt="" />
                                     </div>
-                                    <div className="div_1">
-                                        <p className="wh">HEIGHT</p>
-                                        <li className='height'>{pokemon?.height}</li>
+                                    <div className="boxNumber">
+                                        <p className="number_Pokemon">#{pokemon?.id}</p>
                                     </div>
-                                </ul>
-                                <div className="datails">
-                                    <div className="type">
-                                        <p className='details_pokemon_type'>Type</p>
-                                        {
-                                            <ul className='type_pokemon'>
-                                                {pokemon?.types?.map((typeInfo, index) => (
-                                                    <li className='list_type' key={index}>{typeInfo.type.name}</li>
-                                                ))}
-                                            </ul>
+                                    <h2 className='name_pokemon_selector'>{name}</h2>
+                                    <div className="line"></div>
+                                    <ul className="pokemon_weight_height">
+                                        <div className="div_1">
+                                            <p className="wh">WEIGHT</p>
+                                            <li className='weight'>{pokemon?.weight}</li>
+                                        </div>
+                                        <div className="div_1">
+                                            <p className="wh">HEIGHT</p>
+                                            <li className='height'>{pokemon?.height}</li>
+                                        </div>
+                                    </ul>
+                                    <div className="datails">
+                                        <div className="type">
+                                            <p className='details_pokemon_type'>Type</p>
+                                            {
+                                                <ul className='type_pokemon'>
+                                                    {pokemon?.types?.map((typeInfo, index) => (
+                                                        <li className='list_type' key={index}>{typeInfo.type.name}</li>
+                                                    ))}
+                                                </ul>
 
-                                        }
+                                            }
+                                        </div>
+                                        <div className="hability">
+                                            <p className='details_pokemon_ability'>Ability</p>
+                                            {
+                                                <ul className='type_pokemon'>
+                                                    {pokemon?.abilities.map((abilityInfo, index) => (
+                                                        <li className='list_ability' key={index}>{abilityInfo.ability.name}</li>
+                                                    ))}
+                                                </ul>
+                                            }
+                                        </div>
                                     </div>
-                                    <div className="hability">
-                                        <p className='details_pokemon_ability'>Ability</p>
-                                        {
-                                            <ul className='type_pokemon'>
-                                                {pokemon?.abilities.map((abilityInfo, index) => (
-                                                    <li className='list_ability' key={index}>{abilityInfo.ability.name}</li>
-                                                ))}
-                                            </ul>
-                                        }
                                     </div>
-                                </div>
-                                
-                                <div className="chart">
-                                <p className="stack">STACK</p>
-                                {pokemon && <Chart pokemon={pokemon} />}
-                                </div>    
-                                
-                                <p className="stack">MOVES</p>    
-                                <div className="moves">
-                                
-                                    {pokemon?.moves.map((movesInfo, index) => (
-                                                    <li className='list_moves' key={index}>{movesInfo.move.name}</li>
-                                    ))}
-                                </div>                                                              
-                            </div>                            
+                                </section>
+
+                                <section className="section_stack">
+                                    <p className="stack">STACK</p>
+                                    <div className="chart">                                        
+                                        {pokemon && <Chart pokemon={pokemon} />}
+                                    </div>    
+                                </section>
+                                <section className="section_moves">
+                                <p className="stack">MOVES</p>
+                                    <div className="moves">                                            
+                                            {pokemon?.moves.map((movesInfo, index) => (
+                                            <li className='list_moves' key={index}>{movesInfo.move.name}</li>
+                                        ))}
+                                    </div>
+                                </section>                                                              
+                            
                         </article>
+                        </div>
                     )
             }
 
